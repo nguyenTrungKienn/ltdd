13bf08046db651cad92e20e216b3cb75595f34dd
nguyenTrungKienn <tk624019@gmail.com>
2024-07-17 14:54:15 +0700

    profile

---
+++ app/src/main/java/vlu/android/project/ProfileFragment.java
@@ -0,0 +1,62 @@
+package vlu.android.project;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class ProfileFragment extends Fragment {
+    private LinearLayout profileInfoLayout;
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        profileInfoLayout = view.findViewById(R.id.profile_info_layout);
+
+        // Initially hide the profile info layout
+        profileInfoLayout.setVisibility(View.GONE);
+
+        // Add any button click listeners to show the profile info
+        Button myProfileButton = view.findViewById(R.id.tab_my_profile);
+        myProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profileInfoLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        return view;
+    }
+}
